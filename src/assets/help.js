18dc8cbd7ad6ded64b68defb6c1311b0480f8a0a
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-16 23:40:34 +0600

    updated the app.jsx and new help.js file to keep some data

---
+++ src/assets/help.js
@@ -0,0 +1,14 @@
+{
+  /* Eevee: A Normal-type Pokémon that can evolve into eight different Pokémon, including Jolteon, Flareon, Vaporeon, Umbreon, Espeon, Leafeon, Glaceon, and Sylveon
+    Mewtwo: A Pokémon created by genetic manipulation
+    Celebi: A Psychic/Grass-type Pokémon that is both mythic and legendary
+    Gholdengo: Pokémon number 1,000
+    Mamoswine: A cool Pokémon
+    Deoxys: A cool Pokémon
+    Donphan: A cool Pokémon
+    Mudsdale: A cool Pokémon
+    Trevenant: A cool Pokémon
+    Mew: A cool Pokémon
+    Gogoat: A cool Pokémon
+    Kyurem: An Ice-Dragon Pokémon */
+}
