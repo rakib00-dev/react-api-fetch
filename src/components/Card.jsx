3bab3f4a39abc34fb19149c8b5614328e413ab7f
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-17 01:07:30 +0600

    added image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,12 @@
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="w-max h-max bg-red-500 p-5 rounded-xl">
+      <img
+        src="https://animatorsresourcekit.blog/wp-content/uploads/2020/04/rig-link.png"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Card;
