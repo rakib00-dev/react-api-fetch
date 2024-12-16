606d71d194d0dc6288bdced2b688c6afaeb604ef
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2024-12-17 00:49:42 +0600

    added input content

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Card from './components/Card';
+import Input from './components/Input';
+
 function App() {
-  return <h1 className="bg-red-500 font-medium text-red-50">hellow</h1>;
+  return (
+    <div className="grid place-items-center gap-2 m-8">
+      <Input />
+      <Card />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,13 @@
 const Input = () => {
-  return <div>Input</div>;
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter Your Pokemon Name"
+        className="border-solid border-2 py-2 px-5 rounded-md w-64 mt-7 mb-14"
+      />
+    </>
+  );
 };
 
 export default Input;
